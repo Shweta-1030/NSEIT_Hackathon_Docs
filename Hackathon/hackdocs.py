29316e2a555f78b07b6381ef1d97464495980c0f
Shweta-1030 <140487195+Shweta-1030@users.noreply.github.com>
2023-08-09 12:19:29 +0530

    Update hackdocs.py

--- Hackathon/hackdocs.py
+++ Hackathon/hackdocs.py
@@ -20,7 +20,7 @@ st.markdown("""
 7. Teams can work on an idea that they have worked on before (as long as they do not re-use code).
 8. Teams can use libraries, frameworks, or open-source code in their projects. Working on a project before the event and open-sourcing it for the sole purpose of using the code during the event is against the spirit of the rules and is not allowed.
 9. Teams must stop development once the time is up. However, teams are allowed to debug and make small fixes to their programs after time is up. e.g. If during demoing your project you find a bug that breaks your application and the fix is only a few lines of code, it's okay to fix that. Making large changes or adding new features is not allowed.
-10. Teams that violate above rules are not allowed to participate
+10. Teams that violate above rules are not allowed to participate.
 11. Teams can be disqualified from the competition at the organizers' discretion. Reasons might include but are not limited to breaking the Competition Rules, breaking the or other unsporting behaviour.
 
 """)
