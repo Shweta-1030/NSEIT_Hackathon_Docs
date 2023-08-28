6de359bf1c82b561d77637975bdb8fa9e5cd01c5
Shweta-1030 <140487195+Shweta-1030@users.noreply.github.com>
2023-08-28 15:53:07 +0530

    Update hackdocs.py

--- Hackathon/hackdocs.py
+++ Hackathon/hackdocs.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.set_page_config(layout="wide")
 
 image = Image.open('AI logo.jpg')
-st.image(image, caption=None)
+st.image(image, width=100)
 
 ##st.markdown("(AI)<sup>5</sup>", unsafe_allow_html=True)
 
