500847d03556256f8bf69a7d25d30b64f68a7701
Shweta-1030 <140487195+Shweta-1030@users.noreply.github.com>
2024-02-06 14:04:56 +0530

    Update hackdocs.py

--- Hackathon/hackdocs.py
+++ Hackathon/hackdocs.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-st.title("(AI)5 : Generative AI Hackathon Rules & Guidelines")
+st.title("(AI)5 : Generative AI Hackathon")
 st.subheader('Hackathon Rules & Guidelines')
 st.subheader('Hackathon Use Cases')
