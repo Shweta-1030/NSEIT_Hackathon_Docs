a5bca421a935d07f2c6fdc7e78cf073883d2b5b7
Shweta-1030 <140487195+Shweta-1030@users.noreply.github.com>
2024-02-09 12:57:14 +0530

    Update Hackathon Use Cases.py

--- Hackathon/pages/Hackathon Use Cases.py
+++ Hackathon/pages/Hackathon Use Cases.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import pandas as pd
+from IPython.display import display, HTML
 
 st.set_page_config(layout="wide")
 
 ##st.markdown("(AI)<sup>5</sup>", unsafe_allow_html=True)
-
 st.title("List of Use Cases")
 ##st.markdown("<h1 style='text-align: center; color: grey;'>Big headline</h1>", unsafe_allow_html=True)
 
-df = pd.read_csv("Hackathon/hackathon_use_cases.csv")
+df = pd.read_csv("data/hackathon_use_cases.csv")
+
+#display(HTML(df.to_html(index=False)))
 
 df = st.dataframe(df,hide_index=True)
 
